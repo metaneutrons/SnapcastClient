4bad0cf15dd64e7a4682ccf9e8b743be9bf04908
Craig Sturdy <sturd@protonmail.ch>
2024-04-10 21:49:29 +0100

    Add documentation to public Client functions

--- src/snapcast-net/Client.cs
+++ src/snapcast-net/Client.cs
@@ -1,4 +1,4 @@
-/***
+﻿/***
     This file is part of snapcast-net
     Copyright (C) 2024  Craig Sturdy
 
@@ -127,10 +127,15 @@ public class Client : IClient
 		CommandMutex.ReleaseMutex();
 	}
 
+	/// <summary>
+	/// Retrieves the status of the SnapClient.
+	/// </summary>
+	/// <param name="id">The ID of the client.</param>
+	/// <returns>The status of the client.</returns>
 	public async Task<Models.SnapClient> ClientGetStatusAsync(string id)
 	{
 		var command = CommandFactory.createCommand(CommandType.CLIENT_GET_STATUS, new Params.ClientGetStatus { Id = id });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Client.GetStatus command");
 
 		var tcs = new TaskCompletionSource<ClientStatus>();
@@ -140,15 +145,22 @@ public class Client : IClient
 		return response.Client;
 	}
 
+	/// <summary>
+	/// Sets the volume of a client.
+	/// </summary>
+	/// <param name="id">The ID of the client.</param>
+	/// <param name="volume">The volume level to set.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task ClientSetVolumeAsync(string id, int volume)
 	{
 		var command = CommandFactory.createCommand(CommandType.CLIENT_SET_VOLUME,
-			new Params.ClientSetVolume {
+			new Params.ClientSetVolume
+			{
 				Id = id,
 				Volume = new Params.ClientVolume { Muted = false, Percent = volume }
 			}
 		);
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Client.SetVolume command");
 
 		var tcs = new TaskCompletionSource<VolumeSet>();
@@ -156,21 +168,33 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Sets the latency of a client.
+	/// </summary>
+	/// <param name="id">The ID of the client.</param>
+	/// <param name="latency">The latency value to set.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task ClientSetLatencyAsync(string id, int latency)
 	{
 		var command = CommandFactory.createCommand(CommandType.CLIENT_SET_LATENCY, new Params.ClientSetLatency { Id = id, Latency = latency });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Client.SetLatency command");
-	
+
 		var tcs = new TaskCompletionSource<LatencySet>();
 		Execute(command, new ResponseHandler<LatencySet>(tcs.SetResult, e => tcs.SetException(new CommandException(e))));
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Sets the name of a client.
+	/// </summary>
+	/// <param name="id">The ID of the client.</param>
+	/// <param name="name">The name to set for the client.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task ClientSetNameAsync(string id, string name)
 	{
 		var command = CommandFactory.createCommand(CommandType.CLIENT_SET_NAME, new Params.ClientSetName { Id = id, Name = name });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Client.SetName command");
 
 		var tcs = new TaskCompletionSource<NameSet>();
@@ -178,10 +202,15 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Gets the status of a group.
+	/// </summary>
+	/// <param name="id">The ID of the group.</param>
+	/// <returns>The requested group.</returns>
 	public async Task<Models.Group> GroupGetStatusAsync(string id)
 	{
 		var command = CommandFactory.createCommand(CommandType.GROUP_GET_STATUS, new Params.GroupGetStatus { Id = id });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Group.GetStatus command");
 
 		var tcs = new TaskCompletionSource<GroupStatus>();
@@ -191,10 +220,16 @@ public class Client : IClient
 		return response.Group;
 	}
 
+	/// <summary>
+	/// Sets the mute state of a group.
+	/// </summary>
+	/// <param name="id">The ID of the group.</param>
+	/// <param name="mute">The mute state to set.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task GroupSetMuteAsync(string id, bool mute)
 	{
 		var command = CommandFactory.createCommand(CommandType.GROUP_SET_MUTE, new Params.GroupSetMute { Id = id, Mute = mute });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Group.SetMute command");
 
 		var tcs = new TaskCompletionSource<GroupMuteSet>();
@@ -202,10 +237,16 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Sets the stream for a group.
+	/// </summary>
+	/// <param name="id">The ID of the group.</param>
+	/// <param name="streamId">The ID of the stream to set.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task GroupSetStreamAsync(string id, string streamId)
 	{
 		var command = CommandFactory.createCommand(CommandType.GROUP_SET_STREAM, new Params.GroupSetStream { Id = id, StreamId = streamId });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Group.SetStream command");
 
 		var tcs = new TaskCompletionSource<GroupStreamSet>();
@@ -213,10 +254,16 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Sets the clients of a group.
+	/// </summary>
+	/// <param name="id">The ID of the group.</param>
+	/// <param name="clients">The list of client IDs to set.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task GroupSetClientsAsync(string id, List<string> clients)
 	{
 		var command = CommandFactory.createCommand(CommandType.GROUP_SET_CLIENTS, new Params.GroupSetClients { Id = id, Clients = clients });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Group.SetClients command");
 
 		var tcs = new TaskCompletionSource<GroupClientsSet>();
@@ -224,21 +271,31 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Sets the name of a group.
+	/// </summary>
+	/// <param name="id">The ID of the group.</param>
+	/// <param name="name">The name to set for the group.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task GroupSetNameAsync(string id, string name)
 	{
 		var command = CommandFactory.createCommand(CommandType.GROUP_SET_NAME, new Params.GroupSetName { Id = id, Name = name });
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Group.SetName command");
-	
+
 		var tcs = new TaskCompletionSource<GroupNameSet>();
 		Execute(command, new ResponseHandler<GroupNameSet>(tcs.SetResult, e => tcs.SetException(new CommandException(e))));
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Retrieves the RPC version of the server.
+	/// </summary>
+	/// <returns>The RPC version of the server.</returns>
 	public async Task<Models.RpcVersion> ServerGetRpcVersionAsync()
 	{
 		var command = CommandFactory.createCommand(CommandType.SERVER_GET_RPC_VERSION, new NullParams());
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Server.GetRpcVersion command");
 
 		var tcs = new TaskCompletionSource<RpcVersion>();
@@ -246,10 +303,14 @@ public class Client : IClient
 		return await tcs.Task;
 	}
 
+	/// <summary>
+	/// Retrieves the status of the server.
+	/// </summary>
+	/// <returns>The status of the server.</returns>
 	public async Task<Models.Server> ServerGetStatusAsync()
 	{
 		var command = CommandFactory.createCommand(CommandType.SERVER_GET_STATUS, new NullParams());
-		if(command == null)
+		if (command == null)
 			throw new Exception("Failed to create Server.GetStatus command");
 
 		var tcs = new TaskCompletionSource<ServerStatus>();
@@ -259,6 +320,11 @@ public class Client : IClient
 		return response.Server;
 	}
 
+	/// <summary>
+	/// Deletes a client from the server.
+	/// </summary>
+	/// <param name="id">The ID of the client to delete.</param>
+	/// <returns>A task representing the asynchronous operation.</returns>
 	public async Task ServerDeleteClientAsync(string id)
 	{
 		var command = CommandFactory.createCommand(CommandType.SERVER_DELETE_CLIENT, new Params.ServerDeleteClient { Id = id });
@@ -270,6 +336,11 @@ public class Client : IClient
 		await tcs.Task;
 	}
 
+	/// <summary>
+	/// Adds a stream to the server.
+	/// </summary>
+	/// <param name="streamUri">The URI of the stream to add.</param>
+	/// <returns>The ID of the added stream.</returns>
 	public async Task<string> StreamAddStreamAsync(string streamUri)
 	{
 		var command = CommandFactory.createCommand(CommandType.STREAM_ADD_STREAM, new Params.StreamAddStream { StreamUri = streamUri });
